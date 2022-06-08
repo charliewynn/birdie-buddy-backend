2d18299e8f88b0b11ed5a3a19ba53e4e9480a211
Charlie Wynn <cwynn@affinityplus.org>
2022-06-07 22:01:56 -0500

    adds headers

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "https://cwynn.com",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+    },
     body: JSON.stringify("Hello from Lambda and Github!"),
   };
   return response;
